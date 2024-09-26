f1fa08274a63fd1a08dc379ffb1f9a295bb60101
ZeNoobi <ctaaseen@gmail.com>
2024-09-25 19:37:53 -0700

    Removed Errors From Nil and Con

---
+++ Cons.java
@@ -0,0 +1,45 @@
+public class Cons implements ImmutableList {
+    // ---BEGIN INSTANCE VARIABLES---
+    public final int head;
+    public final ImmutableList tail;
+    // ---END INSTANCE VARIABLES---
+
+    public Cons(final int head, final ImmutableList tail) {
+        this.head = head;
+        this.tail = tail;
+    } // Cons
+
+    public boolean equals(final Object other) {
+        if (other instanceof Cons) {
+            final Cons otherCons = (Cons)other;
+            return head == otherCons.head && tail.equals(otherCons.tail);
+        } else {
+            return false;
+        }
+    } // equals
+    
+    public String toString() {
+        return "Cons(" + head + ", " + tail.toString() + ")";
+    } // toString
+
+    public int hashCode() {
+        return sum();
+    } // hashCode
+
+    public boolean contains(final int value){
+        return true;
+    }
+
+    public int length(){
+        return 0;
+    }
+
+    public int sum(){
+        return 0;
+    }
+
+    public ImmutableList append(final ImmutableList other){
+        
+        return null;
+    }
+} // Cons
---
+++ Nil.java
@@ -0,0 +1,32 @@
+public class Nil implements ImmutableList {
+    public Nil() {}
+
+    public boolean equals(final Object other) {
+        return other instanceof Nil;
+    } // equals
+    
+    public String toString() {
+        return "Nil";
+    } // toString
+
+    public int hashCode() {
+        return 0;
+    } // hashCode
+
+    public boolean contains(final int value){
+        return true;
+    }
+
+    public int length(){
+        return 0;
+    }
+
+    public int sum(){
+        return 0;
+    }
+
+    public ImmutableList append(final ImmutableList other){
+        
+        return null;
+    }
+} // Nil
