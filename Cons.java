433043fe1032a2dd306f72c7faeaa355a4a52029
ZeNoobi <ctaaseen@gmail.com>
2024-10-04 16:36:53 -0700

    First Rendition Cons and Nil

--- Cons.java
+++ Cons.java
@@ -9,37 +9,47 @@ public class Cons implements ImmutableList {
         this.tail = tail;
     } // Cons
 
+    @Override
     public boolean equals(final Object other) {
         if (other instanceof Cons) {
-            final Cons otherCons = (Cons)other;
+            final Cons otherCons = (Cons) other;
             return head == otherCons.head && tail.equals(otherCons.tail);
         } else {
             return false;
         }
     } // equals
     
+    @Override
     public String toString() {
         return "Cons(" + head + ", " + tail.toString() + ")";
     } // toString
 
+    @Override
     public int hashCode() {
-        return sum();
+        return sum(); // Return the sum as the hash code (may not be ideal, but follows the spec)
     } // hashCode
 
-    public boolean contains(final int value){
-        return true;
+    // Recursively calculates the length of the list.
+    @Override
+    public int length() {
+        return 1 + tail.length(); // Head is 1, add the length of the tail
     }
 
-    public int length(){
-        return 0;
+    // Recursively sums the elements of the list.
+    @Override
+    public int sum() {
+        return head + tail.sum(); // Add head to the sum of the tail
     }
 
-    public int sum(){
-        return 0;
+    // Appends another list to this one. Returns a new list.
+    @Override
+    public ImmutableList append(final ImmutableList other) {
+        return new Cons(head, tail.append(other)); // Create a new list with the head and the result of appending `other` to the tail
     }
 
-    public ImmutableList append(final ImmutableList other){
-        
-        return null;
+    // Recursively checks if the value is present in the list.
+    @Override
+    public boolean contains(final int value) {
+        return head == value || tail.contains(value); // Check the head, if not found, check the tail
     }
-} // Cons
+}
--- Nil.java
+++ Nil.java
@@ -1,32 +1,42 @@
 public class Nil implements ImmutableList {
     public Nil() {}
 
+    @Override
     public boolean equals(final Object other) {
         return other instanceof Nil;
     } // equals
-    
+
+    @Override
     public String toString() {
         return "Nil";
     } // toString
 
+    @Override
     public int hashCode() {
-        return 0;
+        return 0; // Nil's hash code is 0
     } // hashCode
 
-    public boolean contains(final int value){
-        return true;
-    }
-
-    public int length(){
+    // Length of an empty list is 0
+    @Override
+    public int length() {
         return 0;
     }
 
-    public int sum(){
+    // Sum of an empty list is 0
+    @Override
+    public int sum() {
         return 0;
     }
 
-    public ImmutableList append(final ImmutableList other){
-        
-        return null;
+    // Appending to an empty list returns the other list
+    @Override
+    public ImmutableList append(final ImmutableList other) {
+        return other; // Nil appended with any list is just that list
     }
-} // Nil
+
+    // An empty list doesn't contain any value
+    @Override
+    public boolean contains(final int value) {
+        return false; // Nil doesn't contain any values
+    }
+}
